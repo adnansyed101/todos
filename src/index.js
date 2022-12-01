02b4e941381fa1dc9e8dfb1a9f4b40b2f3765310
adnansyed101 <sirgeant10@gmail.com>
2022-12-01 14:19:41 +0600

    Created the navbar using materialize css.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min";
+
+M.AutoInit();
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWbpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWbpackPlugin({
+      title: "Todos",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
