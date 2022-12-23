85540d6338ad12612c3e6e653002c65a1fe46c5a
adnansyed101 <sirgeant10@gmail.com>
2022-12-23 14:46:41 +0600

    Add eslint-config-prettier module.
    This module will remove the annoying error messages given by eslint. And also added
    'extend: ['prettier']' to .eslintrc.js per eslint-config-prettier documentation.

--- src/changeTab.js
+++ src/changeTab.js
@@ -1,19 +1,19 @@
 // eslint-disable-next-line space-before-function-paren
 function changeTab(evt, state) {
-  const tabContent = document.getElementsByClassName('tabContent')
+  const tabContent = document.getElementsByClassName("tabContent");
 
   for (let i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = 'none'
+    tabContent[i].style.display = "none";
   }
 
-  const tablinks = document.getElementsByClassName('btn')
+  const tablinks = document.getElementsByClassName("btn");
 
   for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '')
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  document.getElementById(state).style.display = 'block'
-  evt.currentTarget.className += ' active'
+  document.getElementById(state).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-export { changeTab }
+export { changeTab };
