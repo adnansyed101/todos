04a6ec4c11e97ac52bceb5882ce8e4dcd40d7653
adnansyed101 <sirgeant10@gmail.com>
2022-12-23 14:41:27 +0600

    Added eslint for cleaner code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "/src/assets/styles/styles.css";
-import "/src/assets/styles/normalize.css";
-import { changeTab } from "/src/changeTab.js";
+import '../src/assets/styles/styles.css'
+import '../src/assets/styles/normalize.css'
+import { changeTab } from '../src/changeTab.js'
 
-window.changeTab = changeTab;
+window.changeTab = changeTab
 
-document.getElementById("defaultOpen").click();
+document.getElementById('defaultOpen').click()
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const HtmlWbpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWbpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWbpackPlugin({
-      title: "Todos",
-      filename: "index.html",
-      template: "src/template.html",
-    }),
+      title: 'Todos',
+      filename: 'index.html',
+      template: 'src/template.html'
+    })
   ],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
